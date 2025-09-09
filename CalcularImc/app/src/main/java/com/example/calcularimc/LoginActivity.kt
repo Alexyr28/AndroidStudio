49111c064059a399b6f9c1481876793d05d590b4
Alexander Yama Rosero <alexyr24@gmail.com>
2025-09-08 20:53:04 -0500

    feat: Fireba

--- CalcularImc/app/src/main/java/com/example/calcularimc/LoginActivity.kt
+++ CalcularImc/app/src/main/java/com/example/calcularimc/LoginActivity.kt
@@ -1,10 +1,17 @@
 package com.example.calcularimc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +23,69 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //
+        //Elementos
+        val etusernew = findViewById<EditText>(R.id.etUserNew)
+        val etpassword = findViewById<EditText>(R.id.etPassword)
+        val btnlog = findViewById<Button>(R.id.btnLogin)
+        val btnreg = findViewById<Button>(R.id.btnreg)
+
+
+        //Firebase
+        val auth = FirebaseAuth.getInstance()
+
+        //Strings
+        val usernoexist = getString(R.string.usernoexist)
+        val credinvalid = getString(R.string.credinvalid)
+        val error2 = getString(R.string.error2)
+        val campovacio = getString(R.string.campovacio)
+        val regexitoso = getString(R.string.regexitoso)
+        val userexist = getString(R.string.userexist)
+        //---------------------------
+
+        btnlog.setOnClickListener {
+            try {
+                val usuario = etusernew.text.toString()
+                val password = etpassword.text.toString()
+                if (usuario.isNotEmpty() && password.isNotEmpty()){
+                    auth.signInWithEmailAndPassword(usuario, password).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            val paghome = Intent(this, MainActivity::class.java)
+                            startActivity(paghome)
+                            finish()
+                        }else{
+                            if(it.exception is FirebaseAuthInvalidUserException){
+                                Toast.makeText(this,"$usernoexist", Toast.LENGTH_SHORT).show()
+                            }else if (it.exception is FirebaseAuthInvalidCredentialsException){
+                                Toast.makeText(this,"$credinvalid", Toast.LENGTH_SHORT).show()
+                            }else{
+                                Toast.makeText(this,"$error2", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                }else{
+                    Toast.makeText(this,"$campovacio", Toast.LENGTH_SHORT).show()
+                }
+            }catch (e: Exception){
+                Toast.makeText(this,"$error2", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnreg.setOnClickListener {
+            try {
+                val usuario1 = etusernew.text.toString()
+                val password1 = etpassword.text.toString()
+                if(usuario1.isNotEmpty() && password1.isNotEmpty()){
+                    auth.createUserWithEmailAndPassword(usuario1, password1).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            Toast.makeText(this, "$regexitoso", Toast.LENGTH_SHORT).show()
+                        }else{
+                            Toast.makeText(this,"$userexist", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }catch (e: Exception){
+                Toast.makeText(this,"$error2", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- CalcularImc/app/src/main/java/com/example/calcularimc/MainActivity.kt
+++ CalcularImc/app/src/main/java/com/example/calcularimc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.calcularimc
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
@@ -12,6 +13,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -32,6 +34,10 @@ class MainActivity : AppCompatActivity() {
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
         val btnCalcularimc = findViewById<Button>(R.id.btnCalcularimc)
         val btnhistory = findViewById<ImageButton>(R.id.btnhistory)
+        val btnlogout = findViewById<ImageButton>(R.id.btnlogout)
+        // Firebase
+        val auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
         //--------------------------------
         //SharedPreferences
         val sp = getSharedPreferences("Datos", Context.MODE_PRIVATE)
@@ -41,88 +47,102 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //Recuperar Nombre
-        val userName = sp.getString("Name", null)
-        val saludo = getString(R.string.saludo)
-        val tuimc = getString(R.string.tuimc)
-        val tucate = getString(R.string.tucate)
-        val campovacio = getString(R.string.campovacio)
-        val error = getString(R.string.error)
-        //---------------------------
 
-        //Validar Si no hay nombre
-        if (userName == null){
-            val intent1 = Intent(this, UserActivity::class.java)
-            startActivity(intent1)
+        //Validar si esta auntenticado o no
+        if(user == null){
+            val paglog = Intent(this, LoginActivity::class.java)
+            startActivity(paglog)
             finish()
         }else{
-            tvName.text = "$saludo, $userName"
-        }
-        //-----------------------------------
-
-        //SQLITE
-        val dbhelper = SqliteHelper(this)
-        //--------------------------------
-
-        //Click Calcular IMC
-        btnCalcularimc.setOnClickListener {
-            try{
-                val peso = etPeso.text.toString().trim()
-                val altura = etAltura.text.toString().trim()
-
-                if(peso.isNotEmpty() || altura.isNotEmpty()){
-                    val peso = peso.toDouble()
-                    val altura = altura.toDouble()
-                    if(peso == 0.0 || altura == 0.0){
-                        Toast.makeText(this, "$error", Toast.LENGTH_SHORT).show()
-                    }else{
-                        val imc = (peso/(altura*altura))
-
-                        val cate = when {
-                            imc < 18.5 -> "bajo_peso"
-                            imc in 18.5..24.9 -> "peso_saludable"
-                            imc in 25.0..29.9 -> "sobrepeso"
-                            else -> "obesidad"
-                        }
-
-                        val cateTraducida1 = when (cate) {
-                            "bajo_peso" -> getString(R.string.bajo_peso)
-                            "peso_saludable" -> getString(R.string.peso_saludable)
-                            "sobrepeso" -> getString(R.string.sobrepeso)
-                            "obesidad" -> getString(R.string.obesidad)
-                            else -> cate // Si no coincide, dejamos el valor tal cual
-                        }
-
-                        val imcdato = String.format("%.2f",(peso/(altura*altura)))
-                        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-                        val fecha = sdf.format(Date())
-
-                        //IngresarDatos a SQLITE
-                        dbhelper.insertIMC(peso,altura,imc,cate,fecha)
-
-                        tvResultado.text = "$tuimc $imcdato \n $tucate $cateTraducida1"
-                    }
-                }else{
-                    Toast.makeText(this, "$campovacio", Toast.LENGTH_SHORT).show()
-                }
-
-            }catch (e: Exception){
-                Toast.makeText(this, "$error", Toast.LENGTH_SHORT).show()
+            //Recuperar Nombre
+            val userName = sp.getString("Name", null)
+            val saludo = getString(R.string.saludo)
+            val tuimc = getString(R.string.tuimc)
+            val tucate = getString(R.string.tucate)
+            val campovacio = getString(R.string.campovacio)
+            val error = getString(R.string.error)
+            //---------------------------
+            //Validar Si no hay nombre
+            if (userName == null){
+                val intent1 = Intent(this, UserActivity::class.java)
+                startActivity(intent1)
+                finish()
+            }else{
+                tvName.text = "$saludo, $userName"
             }
-        }
-        //---------------------------------------
-        //Click Ir a Editar Campo
-        btnUser.setOnClickListener {
-            val intent3 = Intent(this, UserActivity::class.java)
-            startActivity(intent3)
-            finish()
-        }
-        //--------------------------
-        //Click Ir a Historia
-        btnhistory.setOnClickListener {
-            val inten4 = Intent(this, HistoryActivity::class.java)
-            startActivity(inten4)
-            finish()
+            //-----------------------------------
+
+            //SQLITE
+            val dbhelper = SqliteHelper(this)
+            //--------------------------------
+
+            //Click Calcular IMC
+            btnCalcularimc.setOnClickListener {
+                try{
+                    val peso = etPeso.text.toString().trim()
+                    val altura = etAltura.text.toString().trim()
+
+                    if(peso.isNotEmpty() || altura.isNotEmpty()){
+                        val peso = peso.toDouble()
+                        val altura = altura.toDouble()
+                        if(peso == 0.0 || altura == 0.0){
+                            Toast.makeText(this, "$error", Toast.LENGTH_SHORT).show()
+                        }else{
+                            val imc = (peso/(altura*altura))
+
+                            val cate = when {
+                                imc < 18.5 -> "bajo_peso"
+                                imc in 18.5..24.9 -> "peso_saludable"
+                                imc in 25.0..29.9 -> "sobrepeso"
+                                else -> "obesidad"
+                            }
+
+                            val cateTraducida1 = when (cate) {
+                                "bajo_peso" -> getString(R.string.bajo_peso)
+                                "peso_saludable" -> getString(R.string.peso_saludable)
+                                "sobrepeso" -> getString(R.string.sobrepeso)
+                                "obesidad" -> getString(R.string.obesidad)
+                                else -> cate // Si no coincide, dejamos el valor tal cual
+                            }
+
+                            val imcdato = String.format("%.2f",(peso/(altura*altura)))
+                            val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+                            val fecha = sdf.format(Date())
+
+                            //IngresarDatos a SQLITE
+                            dbhelper.insertIMC(peso,altura,imc,cate,fecha)
+
+                            tvResultado.text = "$tuimc $imcdato \n $tucate $cateTraducida1"
+                        }
+                    }else{
+                        Toast.makeText(this, "$campovacio", Toast.LENGTH_SHORT).show()
+                    }
+
+                }catch (e: Exception){
+                    Toast.makeText(this, "$error", Toast.LENGTH_SHORT).show()
+                }
+            }
+            //---------------------------------------
+            //Click Ir a Editar Campo
+            btnUser.setOnClickListener {
+                val intent3 = Intent(this, UserActivity::class.java)
+                startActivity(intent3)
+                finish()
+            }
+            //--------------------------
+            //Click Ir a Historia
+            btnhistory.setOnClickListener {
+                val inten4 = Intent(this, HistoryActivity::class.java)
+                startActivity(inten4)
+                finish()
+            }
+
+            btnlogout.setOnClickListener {
+                auth.signOut()
+                val logpag = Intent(this, LoginActivity::class.java)
+                startActivity(logpag)
+                finish()
+            }
         }
     }
 }

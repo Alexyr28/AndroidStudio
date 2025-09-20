29d0ae847df259f29e1d46e3fbcd37668973c1d3
Alexyr28 <alexyr24@gmail.com>
2025-09-19 21:56:10 -0500

    feat: correccion splash 2

--- CalcularImc/app/src/main/java/com/example/calcularimc/MainActivity.kt
+++ CalcularImc/app/src/main/java/com/example/calcularimc/MainActivity.kt
@@ -21,7 +21,7 @@ import java.util.Locale
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         Thread.sleep(2000)
-        setTheme(R.style.Theme_CalcularImc)
+        setTheme(R.style.ThemeCalcularImc)
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
